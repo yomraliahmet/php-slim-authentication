f25fc59c11d2c270e8e6de3a2a16d748d67bf414
Ahmet YILDIRIM <ahmetyildirim@live.com>
2018-07-20 11:41:29 +0300

    psr-4 autoload eklendi - "composer dump-autoload -o" komutu çalıştırıldı

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -15,4 +15,5 @@ return array(
     'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
     'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
     'FastRoute\\' => array($vendorDir . '/nikic/fast-route/src'),
+    'App\\' => array($baseDir . '/app'),
 );
